56b662cd577c0bb47ba4ed7ee1738479eae4df97
Pierre Colle <piercus@gmail.com>
2018-04-20 11:36:16 +0200

    fix build

--- build_opencv_static.sh
+++ build_opencv_static.sh
@@ -2,7 +2,7 @@
 export NODE_VERSION=6.10.3
 export OPENCV_VERSION=2.4.13.5
 export DIR=`pwd`
-export LOCAL_SRC=./opencv_src
+export LOCAL_SRC=$DIR/opencv_src
 export OPENCV_RELEASE=${LOCAL_SRC}/opencv-${OPENCV_VERSION}/release
 export OPENCV_INSTALL_PREFIX=${DIR}/build
 
@@ -21,7 +21,7 @@ sudo apt-get install -y build-essential cmake libgtk2.0-dev pkg-config \
                         libxvidcore-dev x264 v4l-utils
 
 ### Download opencv-2.4.13.5
-mkdir -p ${OPENCV_RELEASE} \
+mkdir -p ${OPENCV_RELEASE}
 mkdir -p ${OPENCV_INSTALL_PREFIX}
 cd ${LOCAL_SRC}
 wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
@@ -35,12 +35,12 @@ cmake\
  -D INSTALL_CREATE_DISTRIB=ON\
  -D BUILD_SHARED_LIBS=NO\
  -D CMAKE_INSTALL_PREFIX=${OPENCV_INSTALL_PREFIX}\
- .. \
+ ..
 
 make
 make install
 
-cd ${LOCAL_SRC}\
-rm ${OPENCV_VERSION}.zip\
+cd ${LOCAL_SRC}
+rm ${OPENCV_VERSION}.zip
 rm -rf opencv-${OPENCV_VERSION}
 
